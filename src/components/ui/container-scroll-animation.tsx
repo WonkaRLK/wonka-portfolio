8899bf91997db04aca8255d64b321fb88477ebe0
WonkaRLK <vendedorseba26@gmail.com>
2026-03-21 20:30:22 -0300

    fix: ajustar altura de cards de proyectos en mobile

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -36,7 +36,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[60rem] md:h-[80rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[36rem] sm:h-[50rem] md:h-[80rem] flex items-center justify-center relative p-2 md:p-20"
       ref={containerRef}
     >
       <div
@@ -85,7 +85,7 @@ export const Card = ({
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border border-wonka-chocolate-light p-2 md:p-6 bg-wonka-chocolate rounded-[30px] shadow-2xl"
+      className="max-w-5xl -mt-12 mx-auto h-[13rem] sm:h-[22rem] md:h-[40rem] w-full border border-wonka-chocolate-light p-1 md:p-6 bg-wonka-chocolate rounded-[30px] shadow-2xl"
     >
       <div className="h-full w-full overflow-hidden rounded-2xl bg-wonka-purple-dark md:rounded-2xl">
         {children}
